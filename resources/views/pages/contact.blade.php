3331e52bdd75019a6f375a2d03c9be6e989d4202
Domingos Santos <domingosjmsantos@gmail.com>
2022-11-20 19:10:59 +0000

    contact us

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div>This is the Contact Page</div>
+
+<div id="contact-content">
+    <div class="contact-info">
+        <div>
+            <div>
+                <h2>Address</h2>
+                <p>1234 Street Name</p>
+                <p>City, ST 12345</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h2>Phone</h2>
+                <p>(123) 456-7890</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h2>Email</h2>
+                <p> onlineauctions@auction.com </p>
+            </div>
+        </div> 
+    </div>
+</div>
+
 @endsection
