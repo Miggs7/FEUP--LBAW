fb0dff0fd2af15f71ae884d7143ffe804a03e84c
joao12520 <joaotomas12520@gmail.com>
2022-11-21 10:38:32 +0000

    FAQ and About pages

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div>This is a LBAW project</div>
+<div id="about-content">
+    <div class="about-info">
+        <div>
+            <div>
+                <h2>Made by:</h2>
+                <p>Miguel Tavares, up202002811</p>
+                <p>Gonçalo Ferreira, up202004761</p>
+                <p>Domingos Santos, up201906680</p>
+                <p>João Félix, up202008867</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h3>Online Auction website made for LBAW</h2>
+            </div>
+        </div> 
+    </div>
+</div>
 @endsection
--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.app')
 
-
 @section('content')
-<div>This is a FAQ</div>
+<div id="faq-content">
+    <div class="faq-info">
+        <div>
+            <div>
+                <h2>How much time do I have to pay for my items?</h2>
+                <p>All items must be paid in full within 24 hours of winning the item.</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h2>Do you ship items?</h2>
+                <p>We do not provide shipping, however we will prepare items for shipping and load trucks for a fee. While it is the buyer’s responsibility to arrange shipping, we are happy to provide resources to help facilitate the process.</p>
+            </div>
+        </div> 
+        <div>
+            <div>
+                <h2>Can you help me sell my equipment?</h2>
+                <p>We are a full service company that will make every effort to help you sell your equipment.</p>
+            </div>
+        </div> 
+    </div>
+</div>
 @endsection
